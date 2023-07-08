d9331f7d7b61703cf011b5cb5be007fb29635cde
Dany <danymel1279@gmail.com>
2023-07-07 20:47:59 -0600

    realice la tarea con 2 eje de cada uno

---
+++ traea.php
@@ -0,0 +1,14 @@
+<! DOCTYPE html>
+<html lang="es">
+<cabeza>
+    <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TAREA NUMERO 1</title>
+</cabeza>
+<cuerpo>
+    <h1>INTRODUCCION A JAVA</h1>
+ <script src="./tarea_1.js"></script>
+</cuerpo>
+</.html>
+    
+    
